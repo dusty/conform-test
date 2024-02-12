8d2127d8c052d1f5a38c7a1379f5e1ddb37f2db0
Dusty Doris <dusty@postal.io>
2024-02-12 16:44:00 -0700

    example

--- app/routes/$id.tsx
+++ app/routes/$id.tsx
@@ -1,8 +1,8 @@
 import { getFieldsetProps, getFormProps, getInputProps, useForm } from '@conform-to/react'
 import { parseWithZod } from '@conform-to/zod'
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node'
-import { json, redirect } from '@remix-run/node'
-import { Form, useActionData, useLoaderData, useParams } from '@remix-run/react'
+import { json } from '@remix-run/node'
+import { Form, Link, useActionData, useLoaderData, useParams } from '@remix-run/react'
 import { useDeepCompareEffectNoCheck } from 'use-deep-compare-effect'
 import { z } from 'zod'
 
@@ -40,18 +40,16 @@ export async function loader({ params }: LoaderFunctionArgs) {
   return json(defaultValues.find((d) => d.id === id) ?? {})
 }
 
-export async function action({ request, params }: ActionFunctionArgs) {
+export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData()
   const submission = parseWithZod(formData, { schema: serverSchema })
   if (submission.status !== 'success') {
     const data = submission.reply()
     console.log('error', data)
-    return json(data)
+    return json({ error: data, data: null })
   } else {
     const data = submission.value
-    console.log('success', data)
-    const next = Number(params.id) + 1
-    return redirect(`/${next}`)
+    return json({ data, error: null })
   }
 }
 
@@ -63,7 +61,7 @@ export default function Login() {
   const [form, fields] = useForm({
     // setting a key will reset the form if the params id changes
     id: `user-${params.id}`,
-    lastResult: actionData,
+    lastResult: actionData?.error,
     // load the form data with the initial values from the loader
     defaultValue: { ...loaderData },
     // run html validation client side before submitting
@@ -101,58 +99,92 @@ export default function Login() {
   }, [actionData])
 
   return (
-    <Form
-      method="post"
-      // sets all the accessiblility props
-      {...getFormProps(form)}
-      style={{ marginTop: '1rem' }}
-    >
-      <div id={form.errorId}>{form.errors}</div>
-
-      <div>
-        <label htmlFor={fields.email.id}>Email</label>
-        <input {...getInputProps(fields.email, { type: 'email' })} />
-        <span id={fields.email.errorId}>{fields.email.errors}</span>
-      </div>
-      <div>
-        <label htmlFor={fields.password.id}>Password</label>
-        <input {...getInputProps(fields.password, { type: 'password' })} />
-        <span id={fields.password.errorId}>{fields.password.errors}</span>
-      </div>
-      <label htmlFor={fields.password.id}>
-        <span>Remember me</span>
-        <input {...getInputProps(fields.remember, { type: 'checkbox' })} />
-      </label>
-
-      <fieldset
-        {...getFieldsetProps(fields.thing)}
-        style={{ marginTop: '1rem' }}
-      >
-        <label htmlFor={other.name.id}>Other</label>
-        <input {...getInputProps(other.name, { type: 'text' })} />
-        <span id={other.name.errorId}>{other.name.errors}</span>
-      </fieldset>
-
-      <div style={{ paddingTop: '1rem' }}>
-        {tasks.map((task, index) => {
-          return (
-            <div key={task.key}>
-              <input {...getInputProps(task, { type: 'text' })} />
-              <button {...form.remove.getButtonProps({ name: fields.tasks.name, index })}>
-                remove
-              </button>
-            </div>
-          )
-        })}
-        <button
-          {...form.insert.getButtonProps({ name: fields.tasks.name })}
-          style={{ marginTop: '1rem' }}
+    <div style={{ padding: '1rem' }}>
+      {actionData?.data ? (
+        <div>
+          <p>Success!</p>
+          <Link to={`/${Number(params.id) + 1}`}>Next</Link>
+          <pre>{JSON.stringify(actionData.data, null, 2)}</pre>
+        </div>
+      ) : (
+        <Form
+          method="post"
+          {...getFormProps(form)}
+          style={{ display: 'flex', gap: '1rem', flexDirection: 'column', maxWidth: '600px' }}
         >
-          Add task
-        </button>
-      </div>
+          <div id={form.errorId}>{form.errors}</div>
 
-      <button style={{ marginTop: '1rem' }}>Submit</button>
-    </Form>
+          <div>
+            <label
+              htmlFor={fields.email.id}
+              style={{ display: 'block' }}
+            >
+              Email
+            </label>
+            <input {...getInputProps(fields.email, { type: 'email' })} />
+            <p id={fields.email.errorId}>{fields.email.errors}</p>
+          </div>
+
+          <div>
+            <label
+              htmlFor={fields.password.id}
+              style={{ display: 'block' }}
+            >
+              Password
+            </label>
+            <input {...getInputProps(fields.password, { type: 'password' })} />
+            <p id={fields.password.errorId}>{fields.password.errors}</p>
+          </div>
+
+          <label htmlFor={fields.password.id}>
+            <input {...getInputProps(fields.remember, { type: 'checkbox' })} />
+            <span style={{ marginLeft: '10px' }}>Remember me</span>
+          </label>
+
+          <fieldset
+            {...getFieldsetProps(fields.thing)}
+            style={{ padding: '1rem' }}
+          >
+            <legend>Other</legend>
+            <label
+              htmlFor={other.name.id}
+              style={{ display: 'block' }}
+            >
+              Name
+            </label>
+            <input {...getInputProps(other.name, { type: 'text' })} />
+            <p id={other.name.errorId}>{other.name.errors}</p>
+          </fieldset>
+
+          <div>
+            <p>
+              Tasks{' '}
+              <button
+                {...form.insert.getButtonProps({ name: fields.tasks.name })}
+                style={{ marginLeft: '10px' }}
+              >
+                Add task
+              </button>
+            </p>
+            {tasks.map((task, index) => {
+              return (
+                <div key={task.key}>
+                  <input {...getInputProps(task, { type: 'text' })} />
+                  <button
+                    {...form.remove.getButtonProps({ name: fields.tasks.name, index })}
+                    style={{ margin: '10px' }}
+                  >
+                    remove
+                  </button>
+                  <span id={task.errorId}>{task.errors}</span>
+                </div>
+              )
+            })}
+          </div>
+
+          <button style={{ marginTop: '1rem' }}>Submit</button>
+        </Form>
+      )}
+    </div>
   )
 }
